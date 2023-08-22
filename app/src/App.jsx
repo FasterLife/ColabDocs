8af0b982219716287278e117dae2fb8bc7c99d5d
FayazRahman <fayazrahman4u@gmail.com>
2023-08-22 17:54:59 +0530

    save button

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -15,6 +15,40 @@ const Button = styled.button`
   margin: 10px 10px;
 `
 
+/**
+ * Stores the the text in a local file. Usually the web browser determines where the file is stored (download folder).
+ *
+ * @param text The content of the file.
+ * @param fileName The name of the target file (should not contain a path).
+ */
+export const saveTextAsFile = (text, fileName) => {
+  const blob = new Blob([text], { type: "text/plain" });
+  const downloadLink = document.createElement("a");
+  downloadLink.download = fileName;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL) {
+      // No need to add the download element to the DOM in Webkit.
+      downloadLink.href = window.webkitURL.createObjectURL(blob);
+  } else {
+      downloadLink.href = window.URL.createObjectURL(blob);
+      downloadLink.onclick = (event) => {
+          if (event.target) {
+              document.body.removeChild(event.target);
+          }
+      };
+      downloadLink.style.display = "none";
+      document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
+
+  if (window.webkitURL) {
+      window.webkitURL.revokeObjectURL(downloadLink.href);
+  } else {
+      window.URL.revokeObjectURL(downloadLink.href);
+  }
+};
+
 // Setup Monaco Editor
 // Attach YJS Text to Monaco Editor
 
@@ -47,6 +81,7 @@ function App() {
         theme="vs-dark"
         onMount={handleEditorDidMount}
       />
+      <Button onClick={() => saveTextAsFile(editorRef.current.getValue(), "test.txt")}>Save</Button>
     </div>
   )
 }
